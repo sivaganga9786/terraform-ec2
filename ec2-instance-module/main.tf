ea1cf054884f91c78362f1f8b345d72a63a6fac2
siva <veeraganesh6@gmail.com>
2024-09-25 18:18:26 +0530

    added ssh secrets

--- ec2-instance-module/main.tf
+++ ec2-instance-module/main.tf
@@ -1,14 +1,3 @@
-#module "ec2" {
-#  source             = "../ec2-instance"
-#  ami                = var.ami  # Replace with the desired AMI ID
-#  instance_type      = var.instance_type
-#  subnet_id          = module.vpc.public_subnet_id  # Use public subnet from the VPC module
-#  ec2_instance_name  = var.ec2_instance_name
-#  vpc_id             = module.vpc.vpc_id
-#  ec2_sg_name        = var.ec2_sg_name
-#}
-
-
 module "ec2" {
   source         = "../ec2-instance-resources"
   ami         = var.ami                      # Replace with your AMI ID
@@ -17,4 +6,5 @@ module "ec2" {
   vpc_id         = data.aws_vpc.existing_vpc.id
   ec2_instance_name  = var.ec2_instance_name
   ec2_sg_name        = var.ec2_sg_name
+  key_name            = ec2-ssh
 }

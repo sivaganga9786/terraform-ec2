df459b8b9387fc084952e09b2bfed58fda5e6693
siva <veeraganesh6@gmail.com>
2024-09-25 17:51:46 +0530

    added github secrets

--- ec2-instance-module/provider.tf
+++ ec2-instance-module/provider.tf
@@ -2,11 +2,11 @@
 
 provider "aws" {
   region            = var.region
-  access_key = data.vault_generic_secret.aws_access.data["access_key"]
-  secret_key = data.vault_generic_secret.aws_access.data["secret_key"]
+#  access_key = data.vault_generic_secret.aws_access.data["access_key"]
+#  secret_key = data.vault_generic_secret.aws_access.data["secret_key"]
 }
 
 provider "vault" {
   address = "http://127.0.0.1:8200"
-  token = var.token
+ # token = var.token
 }

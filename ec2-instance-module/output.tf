d4efabfa9caa023506309855073d0666d216953e
siva <veeraganesh6@gmail.com>
2024-09-25 17:50:22 +0530

    added github secrets

--- ec2-instance-module/output.tf
+++ ec2-instance-module/output.tf
@@ -8,12 +8,12 @@ output "ec2_sg_id" {
   value       = module.ec2.security_group_id
 }
 
-output "aws_access_key" {
-  value = data.vault_generic_secret.aws_access.data["access_key"]
-  sensitive = true
-}
+#output "aws_access_key" {
+#  value = data.vault_generic_secret.aws_access.data["access_key"]
+#  sensitive = true
+#}
 
-output "aws_secret_key" {
-  value = data.vault_generic_secret.aws_access.data["secret_key"]
-  sensitive = true
-}
+#output "aws_secret_key" {
+#  value = data.vault_generic_secret.aws_access.data["secret_key"]
+#  sensitive = true
+#}
